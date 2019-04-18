965f4e25bc6b494444aa42c8657475666ca0001e
Graeme Porteous <graeme@rgbp.co.uk>
2019-04-18 09:22:37 +0100

    Include properties details in scrape

--- scraper.rb
+++ scraper.rb
@@ -6,10 +6,12 @@ auths = UKPlanningScraper::Authority.tagged('london')
 params = %w(validated_days decided_days)
 
 auths.each_with_index do |auth, i|
+  next unless auth.tagged?('idox')
+
   params.each_with_index do |param, j|
     puts "Authority #{i + 1} of #{auths.size}: Scrape #{j + 1} of #{params.size} for #{auth.name}."
     begin
-      apps = auth.send(param, ENV['MORPH_DAYS'].to_i).scrape
+      apps = auth.send(param, ENV['MORPH_DAYS'].to_i).include_property.scrape
       ScraperWiki.save_sqlite([:authority_name, :council_reference], apps)
       puts "#{auth.name}: #{apps.size} application(s) saved."
     rescue StandardError => e
